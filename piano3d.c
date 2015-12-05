daae70595b66299fa944df235aa2e9827e9125ae
tniles <tniles09@gmail.com>
2015-12-04 23:42:45 -0800

    PIN CHANGE! Changes to 12MHz clock (+/- 0.2%) from FTDI chip. This necessitates rearranging cathode pinouts--free the XTAL1 (pb6) for use as clk_in, moving former CATx on pb6 to pb7, and former CATx+1 on pb7 to pd4. LEDTEST removed. RC values updated. PortB7 reset to 1 when moving on to PortD.
    Tested and proven at 19.2k baud.

--- piano3d.c
+++ piano3d.c
@@ -1,5 +1,6 @@
 /***********************************************************
- * piano3d_R2.c 
+ * piano3d_R3.c 
+ * USES EXTERNAL 12MHz CLOCK FROM FTDI CHIP
  *
  * Tyler Niles
  * 4/15/2010
@@ -22,30 +23,27 @@
  * cathode, within the keyboard) necessitate AN0-5 to be 
  * pulled high and read as CAT0-10 are sunk in progression.
  *
- * I/O lines ANx are pulled high to Vcc with 10k resistors
- * and also decoupled to Gnd with 220pF capacitors.
+ * I/O lines ANx are pulled high to Vcc with 47k resistors
+ * and also decoupled to Gnd with 10pF capacitors.
  **********************************************************/
 
 #include <avr/io.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CALBYTE	0x83;		// OSCCAL byte value
-#define F_CPU	8000000UL	// Clock speed (8MHz CKSEL3..0=0010)
+#define F_CPU	12000000UL	// Clock speed (8MHz CKSEL3..0=0010)
 #include <util/delay.h>
 
-//#define LEDTEST   1		// test with LEDs (no UART) 
 
 #define NUMKEYS  61		// 61 piano keys
 #define ANODES    6		//  6 anodes
 #define CATHODES 11		// 11 cathodes
-
-#define TESTODE   5		// cat# to test for LEDTEST
+#define PULLTIME  3		// delay time (us) for pulling cats low
 
 // Flow control
 #define RTS_n	(PIND & (1<<PIND2))	// Input from FTDI chip
 //#define CTS_n	PORTD.PD3		// Output to FTDI chip
-#define CTS_n_mask	0xF7		// & to send pd3 low
+#define CTS_n_mask	0xF7		// & to send pd3 low to assert
 
 // Read these Pins
 #define AN0	(PINC & (1<<PINC0))	//c1
@@ -63,9 +61,11 @@
 #define CAT3	PORTB.PB3	//c15
 #define CAT4	PORTB.PB4	//c17
 #define CAT5	PORTB.PB5	//c 6
-#define CAT6	PORTB.PB6	//c 4
-#define CAT7	PORTB.PB7	//c 8
+		//#define CAT6	PORTB.PB6	//c 4	// freed up for xtal 1
+#define CAT6	PORTB.PB7	//c 4
+		//#define CAT7	PORTB.PB7	//c 8
 //PortD
+#define CAT7	PORTD.PD4	//c 8
 #define CAT8	PORTD.PD5	//c10
 #define CAT9	PORTD.PD6	//c12
 #define CAT10	PORTD.PD7	//c14
@@ -94,7 +94,7 @@ void sendkeylist(unsigned int packet){
 	KEYS data;		//temp var
 	data = keylist;		//grab global values as-is
 
-	USART_Transword(packet); 		 // send packet# - 2bytes
+	//USART_Transword(packet); 		 // send packet# - 2bytes
 
 	USART_Transbyte((char) (data & 0xFF));   // send keydata - 1byte (LSB)
 	data >>= 8;
@@ -113,7 +113,7 @@ void sendkeylist(unsigned int packet){
 	data >>= 8;
 	USART_Transbyte((char) (data & 0xFF));	 // byte #8		 (MSB)
 
-	/* Total Packet Size: 10 bytes */
+	/* Total Packet Size: 8 bytes */
 }
 
 
@@ -121,36 +121,30 @@ void getkeylist(void){
 
 	/* Set bits of keys 1 or 0 if pressed or released */
 	unsigned char bufbyte, bufbyte2;
-#ifdef LEDTEST
-       	unsigned char ledbuf;
-#endif
 	unsigned int i;
 	KEYS data;		// temp var for keylist
 	KEYS shifter;		// var for shifting mask
 
 	/* Read AN0-5 as CAT0-10 are pulled low in succession */
-	/* During LEDTEST, LEDs are considered on PortD, whereas */
-	/* in normal ops, Tx/Rx pins are considered on PortD */
+	/* In normal ops, Tx/Rx pins are considered on PortD */
 
 	data   = 0x0000000000000000;		// assume all keys NOT pressed
 	shifter= 0x0000000000000001;		// shifting a 1 for masking
 
-#ifndef LEDTEST
 	/* FULL Keyboard Read */
 	i = 0;  bufbyte = 0xFF;  PORTB = bufbyte & ~(1<<i);
-	_delay_us(2);
-	if(!AN0)    data = shifter;		// Special case @ i=0
-	for(i=1; i<CATHODES; i++){		// Start at 1 to avoid special case
-	  if(i<8){
-		bufbyte  = 0xFF;		// default portB config
-		PORTB = bufbyte  & ~(1<<i);     // send low one cathode group
-	  }
-	  else{
-		bufbyte2 = PIND | 0xE0;		// portD Rx/Tx, Flow Ctrl pins considered, adjust pd5,6,7
-		PORTD = bufbyte2 & ~(1<<(i-3));	// pd5,6,7 for CAT8,9,10
-	  }
-
-	  _delay_us(5);			// NEED this delay here! (time to pull cathode low)
+	_delay_us(PULLTIME);
+	_delay_us(0);
+	_delay_us(0);
+	_delay_us(0);
+	if(!AN0)    data = shifter;	// Special case @ cathode i=0
+	for(i=1; i<6; i++){		// i for cathodes 1-5
+	  bufbyte  = 0xFF;		// default portB config
+	  PORTB = bufbyte  & ~(1<<i);   // send low one cathode group
+	  _delay_us(PULLTIME);			// NEED this delay here! (time to pull cathode low)
+	  _delay_us(0);
+	  _delay_us(0);
+	  _delay_us(0);
 	  if(!AN0)  data += (shifter<<(i*ANODES-0));	// cathode_index * 6_anodes_per - anode_num
 	  if(!AN1)  data += (shifter<<(i*ANODES-1));	//
 	  if(!AN2)  data += (shifter<<(i*ANODES-2));	//
@@ -158,59 +152,57 @@ void getkeylist(void){
 	  if(!AN4)  data += (shifter<<(i*ANODES-4));	//
 	  if(!AN5)  data += (shifter<<(i*ANODES-5));	//
 	}
-#else
-	/* FULL Keyboard test with LEDs */
-	ledbuf=0xFF;				// buffer for LEDs
-	i = 0;  bufbyte = 0xFF;  PORTB = bufbyte & ~(1<<i);
-	if(!AN0)    data = 1;			// Special case @ i=0
-	for(i=1; i<CATHODES; i++){		// Start at 1 to avoid special case
-	  if(i<8){
-		bufbyte  = 0xFF;		// default portB config
-		PORTB = bufbyte  & ~(1<<i);	// send low one cathode group
-	  }
-	  else{
-		bufbyte2 = PIND & 0xFF;		// portD leds considered, adjust pd5,6,7
-		PORTD = bufbyte2 & ~(1<<(i-3));	// pd5,6,7 for CAT8,9,10
-	  }
 
-	  _delay_us(3);			// NEED this delay here! (time to pull cathode low)
-	  if(!AN0){  data += (1<<(i*ANODES-0));	ledbuf &= 0xFE; } // cathode_index * 6_anodes_per - anode_num
-	  if(!AN1){  data += (1<<(i*ANODES-1));	ledbuf &= 0xFD;	} // and set leds...
-	  if(!AN2){  data += (1<<(i*ANODES-2));	ledbuf &= 0xFB;	} //
-	  if(!AN3){  data += (1<<(i*ANODES-3));	ledbuf &= 0xF7;	} //
-	  if(!AN4){  data += (1<<(i*ANODES-4));	ledbuf &= 0xEF;	} //
-	  if(!AN5){  data += (1<<(i*ANODES-5));	}		  // ignore 6th (only 5 leds)
+	//i=6;				// i should be 6 here (6th cathode)
+	bufbyte  = 0xFF;		// default portB config
+	PORTB = bufbyte  & ~(1<<(i+1));     // send low PB7
+	_delay_us(PULLTIME);
+	_delay_us(0);
+	_delay_us(0);
+	_delay_us(0);
+	  if(!AN0)  data += (shifter<<(i*ANODES-0));	// cathode_index * 6_anodes_per - anode_num
+	  if(!AN1)  data += (shifter<<(i*ANODES-1));	//
+	  if(!AN2)  data += (shifter<<(i*ANODES-2));	//
+	  if(!AN3)  data += (shifter<<(i*ANODES-3));	//
+	  if(!AN4)  data += (shifter<<(i*ANODES-4));	//
+	  if(!AN5)  data += (shifter<<(i*ANODES-5));	//
+	PORTB = 0xFF;			// reset portB
+
+	for(i=7; i<CATHODES; i++){	// i continues at 7
+	  bufbyte2 = PIND | 0xF0;		// portD Rx/Tx, Flow Ctrl pins considered, adjust pd5,6,7
+	  PORTD = bufbyte2 & ~(1<<(i-3));	// pd4,5,6,7 for CAT7,8,9,10
+	  _delay_us(PULLTIME);				// NEED this delay here! (time to pull cathode low)
+	  _delay_us(0);
+	  _delay_us(0);
+	  _delay_us(0);
+	  if(!AN0)  data += (shifter<<(i*ANODES-0));	// cathode_index * 6_anodes_per - anode_num
+	  if(!AN1)  data += (shifter<<(i*ANODES-1));	//
+	  if(!AN2)  data += (shifter<<(i*ANODES-2));	//
+	  if(!AN3)  data += (shifter<<(i*ANODES-3));	//
+	  if(!AN4)  data += (shifter<<(i*ANODES-4));	//
+	  if(!AN5)  data += (shifter<<(i*ANODES-5));	//
 	}
-#endif
 
 	keylist = data;			// set key list
 
 	/* RESET CATHODES */
-	PORTB = 0xFF;
-#ifndef LEDTEST
-	PORTD = PIND | 0xE0;		// 'OR' due to tx/rx, flow ctrl pins
-#else
-	//PORTD = PIND & 0xFF;		// 'AND' due to active low LEDs
-	PORTD = ledbuf;			// write LEDs to port
-#endif
+	//PORTB = 0xFF;			// Already done... see above
+	PORTD = PIND | 0xF0;		// 'OR' due to tx/rx, flow ctrl pins
 
 }
 
 
 void init(void){
 
-// Internal RC Oscillator
-#ifdef CALBYTE
-OSCCAL=CALBYTE;	 // tune
-#endif
+// External Oscillator from FTDI chip (on pb6/xtal1)
 CLKPR=0x80;
 CLKPR=0x00;  // set clock div factor to 1
 
 // Port B
-// Func7=Out Func6=Out Func5=Out Func4=Out Func3=Out Func2=Out Func1=Out Func0=Out
-// State7=1 State6=1 State5=1 State4=1 State3=1 State2=1 State1=1 State0=1 
+// Func7=Out Func6=In Func5=Out Func4=Out Func3=Out Func2=Out Func1=Out Func0=Out
+// State7=1 State6=P State5=1 State4=1 State3=1 State2=1 State1=1 State0=1 
 PORTB=0xFF;
-DDRB=0xFF;
+DDRB=0xBF;
 
 // Port C
 // Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
@@ -219,15 +211,10 @@ PORTC=0x00;
 DDRC=0x00;
 
 // Port D
-// Func7=Out Func6=Out Func5=Out Func4=In Func3=Out Func2=In Func1=In Func0=In
-// State7=1  State6=1  State5=1  State4=T State3=1  State2=P State1=T State0=T 
-#ifndef LEDTEST
-PORTD=0xE4;
-DDRD=0xE8;
-#else
-PORTD=0xFF;
-DDRD=0xFF;
-#endif
+// Func7=Out Func6=Out Func5=Out Func4=Out Func3=Out Func2=In Func1=In Func0=In
+// State7=1  State6=1  State5=1  State4=1 State3=1  State2=P State1=T State0=T 
+PORTD=0xFC;
+DDRD=0xF8;
 
 // USART
 // Communication Parameters: 8 Data, 1 Stop, No Parity
@@ -235,19 +222,11 @@ DDRD=0xFF;
 // USART Transmitter	   : ON
 // USART0 Mode		   : Asynchronous
 // USART Baud Rate	   : 250k
-#ifndef LEDTEST
-UCSR0A=0x00;
-UCSR0B=0x18;	//enable tx/rx
+UCSR0A=0x00;	// bit 1 is U2x0... set to 2xspeed, clear for 1xspeed
+UCSR0B=0x18;	//enable rx bit 4 - enable tx bit 3
 UCSR0C=0x06;
 UBRR0H=0x00;
-UBRR0L=0x19;	// @ 8MHz: 0x33 for 9600baud, 0x19 for 19.2k, 0x0C for 38.4k, 0x01 for 250k, 0x00 for 0.5M
-#else
-UCSR0A=0x00;
-UCSR0B=0x00;
-UCSR0C=0x06;
-UBRR0H=0x00;
-UBRR0L=0x33;
-#endif
+UBRR0L=0x26;	// @12MHz: 0x26 for 19.2k, 0x12 for 38.4k, 0x05 for 115.2k, 0x04 for 150k, 0x02 for 250k, 0x00 for 750k
 
 // Analog Comparator
 // Analog Comparator: OFF
@@ -277,22 +256,15 @@ int main(void){
   flag=1;			// move to timer interrupt?
   i=0;				// init packet counter
 
-#ifndef LEDTEST
-    PORTD = PIND & CTS_n_mask;	// assert clear to send
-#endif
+  PORTD = PIND & CTS_n_mask;	// assert clear to send
   while(1){
-
-    _delay_us(200);	// check keys this often... or use flag
+    _delay_us(25);	// check keys this often... or use flag
     getkeylist();
-#ifndef LEDTEST
-    //PORTD = PIND & CTS_n_mask;	// assert clear to send
     if(!RTS_n){				// if RTS asserted, send
       PORTD = PIND | 0x08;		// de-assert clear to send
       sendkeylist(i++);			//
       PORTD = PIND & CTS_n_mask;	// assert clear to send
     }
-#endif
-
   }
 
   return 0;
